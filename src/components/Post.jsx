7ef82b2525766458cbada3ace9aafdae230a1bbc
Bartek <bartek.batko3@gmail.com>
2023-08-26 15:39:07 +0200

    .

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,9 @@ import postStyles from "./Post.module.scss";
 const Post = (props) => {
   return (
     <div className={postStyles.post}>
-      <p>{props.text}</p>
+      <h2 className={postStyles.name}>{props.name}</h2>
+      <p className={postStyles.text}>{props.text}</p>
+      <p className={postStyles.time}>{props.time}</p>
     </div>
   );
 };
--- src/components/PostsSection.jsx
+++ src/components/PostsSection.jsx
@@ -3,18 +3,18 @@ import postSectionStyles from './PostsSection.module.scss'
 import Post from './Post'
 
 const DUMMY_POSTS = [
-    {text: 'test'},
-    {text: 'test2'},
-    {text: 'test3'},
-    {text: 'test3'},
-    {text: 'test3'},
+    {name: 'name', text: 'test1', time: '12:00'},
+    {name: 'name', text: 'test2', time: '12:00'},
+    {name: 'name', text: 'test3', time: '12:00'},
+    {name: 'name', text: 'test3', time: '12:00'},
+    {name: 'name', text: 'test3', time: '12:00'},
 ]
 
 const PostsSection = () => {
   return (
     <div className={postSectionStyles.postSection}>{
         DUMMY_POSTS.map(post => {
-            return <Post text={post.text}/>
+            return <Post text={post.text} name={post.name} time={post.time}/>
         })
     }</div>
   )
